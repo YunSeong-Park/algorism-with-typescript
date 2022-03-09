049de012fbe160d523fe3b42bc02e02fce5c4a7d
YunSeong Park <qkrdbstjd95@naver.com>
2022-03-09 12:47:59 +0900

    [New Feature] tail call fibonacci function

--- visualization/src/argorithm/fibonacci/fibonacci.test.ts
+++ visualization/src/argorithm/fibonacci/fibonacci.test.ts
@@ -1,4 +1,4 @@
-import { fib } from "./fibonacci";
+import { fib, tailCallFib } from "./fibonacci";
 
 function fibequation(n: number) {
   return (
@@ -27,6 +27,26 @@ describe("fibonacci", () => {
       expect(fib(10)).toBe(fibequation(10));
 
       expect(fib(20)).toBe(fibequation(20));
+    });
+  });
+
+  describe("tail call function", () => {
+    test("음수는 0으로 반환", () => {
+      expect(tailCallFib(-100)).toBe(0);
+    });
+
+    test("0 = 0", () => {
+      expect(tailCallFib(0)).toBe(0);
+    });
+
+    test("1 = 1", () => {
+      expect(tailCallFib(1)).toBe(1);
+    });
+
+    test("10, 20", () => {
+      expect(tailCallFib(10)).toBe(fibequation(10));
+
+      expect(tailCallFib(20)).toBe(fibequation(20));
     });
   });
 });
--- visualization/src/argorithm/fibonacci/fibonacci.ts
+++ visualization/src/argorithm/fibonacci/fibonacci.ts
@@ -11,3 +11,24 @@ export function fib(n: number): number {
 
   return fib(n - 1) + fib(n - 2);
 }
+
+export function tailCallFib(
+  n: number,
+  prev: number = 1,
+  twoPrev: number = 0
+): number {
+  if (n < 1) {
+    return twoPrev;
+  }
+
+  if (n === 1) {
+    return prev;
+  }
+
+  const curr = prev + twoPrev;
+
+  twoPrev = prev;
+  prev = curr;
+
+  return tailCallFib(n - 1, prev, twoPrev);
+}
