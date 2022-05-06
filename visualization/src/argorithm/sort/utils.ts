70172aa84194cd277a9e2aafc233df440b0e6747
YunSeong Park <qkrdbstjd95@naver.com>
2022-05-06 19:47:13 +0900

    [Bug Fix] 같은 값으로 인자를 받은 경우 값을 지우는 버그 픽스

--- visualization/src/argorithm/sort/utils.ts
+++ visualization/src/argorithm/sort/utils.ts
@@ -1,4 +1,7 @@
 export function swapItem(array: number[], i: number, j: number): void {
+  if (i === j) {
+    return;
+  }
   array[i] ^= array[j];
   array[j] ^= array[i];
   array[i] ^= array[j];
